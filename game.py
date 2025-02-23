22ef0259ce3d9a4d3c097fec4b59b773fe105c17
Марат <108427651+kamaramak@users.noreply.github.com>
2025-02-23 20:57:54 +0300

    Update game.py

--- game.py
+++ game.py
@@ -16,10 +16,10 @@ def main():
     if row < 0 or row >= game.field_size:
         # ...выбросить исключение FieldIndexError.
         raise FieldIndexError
+    column = int(input('Введите номер столбца: '))
     if column < 0 or column >= game.field_size:
         # ...выбросить исключение FieldIndexError.
         raise FieldIndexError
-    column = int(input('Введите номер столбца: '))
     game.make_move(row, column, 'X')
     print('Ход сделан!')
     game.display()
