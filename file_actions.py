9e77ef5e1aaa9cfeb6b771330ba3daee173085c3
Марат Кабардоков <kabardokov.m@yandex.ru>
2025-02-24 01:03:45 +0300

    a

---
+++ file_actions.py
@@ -0,0 +1,8 @@
+# file_actions.py
+
+# Открыть на запись файл example.txt
+file = open('example.txt', 'w', encoding='utf-8')
+# Записать в файл строку.
+file.write('Зевну, укроюсь с головою,\nбудильник заведу на март.\n')
+# Закрыть файл.
+file.close()
