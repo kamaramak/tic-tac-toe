2e3035ac77b5c835fd5a137c231329f3b84955eb
Марат <108427651+kamaramak@users.noreply.github.com>
2025-02-23 20:54:02 +0300

    Update game.py

--- game.py
+++ game.py
@@ -1,16 +1,25 @@
 # game.py
+
 from gameparts import Board
+# Из файла exceptions.py, который лежит в пакете gameparts,
+# импортируется класс FieldIndexError.
+from gameparts.exceptions import FieldIndexError
 
-
-# Вот новая функция.
 def main():
     game = Board()
     game.display()
-    game.make_move(1, 1, 'X')
+    # Пользователь вводит номер строки.
+    row = int(input('Введите номер строки: '))
+    # Если введённое значение меньше нуля или больше или равно
+    # field_size (это значение равно трём, оно хранится в модуле
+    # parts.py)...
+    if row < 0 or row >= game.field_size:
+        # ...выбросить исключение FieldIndexError.
+        raise FieldIndexError
+    column = int(input('Введите номер столбца: '))
+    game.make_move(row, column, 'X')
     print('Ход сделан!')
     game.display()
 
-
-# А вот вызов этой функции.
 if __name__ == '__main__':
     main()
